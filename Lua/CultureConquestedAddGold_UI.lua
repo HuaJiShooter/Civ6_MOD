043be01faac4b03fe1f48768e661c096cc50b308
LAPTOP-CPFL7U2B\14830 <1483019420@qq.com>
2023-02-23 12:49:02 +0800

    STARRY自动主体化完成

--- Lua/CultureConquestedAddGold_UI.lua
+++ Lua/CultureConquestedAddGold_UI.lua
@@ -13,15 +13,17 @@ function CultureConquestedAddGold()
 			
 			local NumVisitingUs = pPlayer:GetCulture():GetTouristsTo()
 			pPlayer = Players[value]
+			local PlayerNumber = 0.0
 		
 			for rowOtherCivIndex in pairs(AlivePlayerList) do
 				local OtherCivIndex = rowOtherCivIndex - 1
 				local OtherCiv = Players[OtherCivIndex]
 				local NumStaycationers = OtherCiv:GetCulture():GetStaycationers()
+				PlayerNumber = PlayerNumber + 1.0
 				
 				if NumStaycationers < NumVisitingUs then
 					local OtherCivYield = OtherCiv:GetTreasury():GetGoldYield()
-					local ChangeAmount = OtherCivYield*0.1
+					local ChangeAmount = OtherCivYield*(0.5/PlayerNumber)
 					ExposedMembers.BOCCHIConquest.ChangeGoldBalanceFromConquest(value, ChangeAmount)
 				end
 			end
